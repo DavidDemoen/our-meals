5cf8d537ce61b6542c1f26edf86c4885fa1ffe9b
DAVID DEMOEN <david.demoen@student.hogent.be>
2026-05-02 10:38:31 +0200

    david - added bootstrap

--- src/application/bootstrap.py
+++ src/application/bootstrap.py
@@ -1,4 +1,15 @@
+import uvicorn
+
 from src.api.app import create_fast_api_app
 
 
 app = create_fast_api_app()
+
+
+def run() -> None:
+    """Run the application."""
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+    )
